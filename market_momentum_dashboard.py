e0d1399fab5065d0f40eba0af8d5fd30311502fe
cboedow <cboedow@gmail.com>
2025-08-02 13:38:39 -0500

    Update market_momentum_dashboard.py

--- market_momentum_dashboard.py
+++ market_momentum_dashboard.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import scipy.interpolate as si
 import warnings
-from RRGpy import RRG
+from RRGPy import RRG
 from RRG.universe import Universe
 
 
